88f148ac242b568c3501706aa2aee623131cc319
liang <liangn@yonyou.com>
2021-08-11 16:20:52 +0800

    feat:修改list

--- src/container/List/index.js
+++ src/container/List/index.js
@@ -5,15 +5,14 @@ import { UserContext } from '~/store'
 import avatarImg from '../../images/avatar.jpg'
 import './style.less'
 
-const List = () => {
+export default () => {
   useTitle('测试')
   const { user, resetUser } = useContext(UserContext)
   return (
     <div className="list">
       姓名:{user.name} 年龄：{user.age}
       <a onClick={() => resetUser({ age: 22 })}>切换年龄</a>
+      <img src={avatarImg} alt="" />
     </div>
   )
 }
-
-export default List
